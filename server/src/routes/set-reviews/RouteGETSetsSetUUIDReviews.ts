c5e76499217c67130ee489ac56277f9ec983fe48
AJGamesArchive <68122156+AJGamesArchive@users.noreply.github.com>
2024-11-24 18:56:07 +0000

    Server: Temp Removed 404 From '/sets/{setUUID}/reviews' Endpoint To Prevent A 404 Error When No Reviews Have Been Posted For The Set

--- server/src/routes/set-reviews/RouteGETSetsSetUUIDReviews.ts
+++ server/src/routes/set-reviews/RouteGETSetsSetUUIDReviews.ts
@@ -7,8 +7,10 @@ import getSetReviews, {
 import {
 	GETSetsSetUUIDReviewsParams,
 	GETSetsSetUUIDReviewsReply200,
-	GETSetsSetUUIDReviewsReplyError,
+	// GETSetsSetUUIDReviewsReplyError,
 } from '../../schemas/set-reviews/SchemaGETSetsSetUUIDReviews.js';
+
+//TODO Find a better way to detecting 'Set Not Found' and re-add 404 if time permits
 
 /**
  * Route to fetch all set reviews from the DB
@@ -19,12 +21,6 @@ const routeGETSetSetUUIDReviews = async (
 ): Promise<void> => {
 	// Fetch reviews
 	const allReviews: FullSetReview[] = await getSetReviews(req.params.setUUID);
-	if (allReviews.length === 0) {
-		rep.status(404).send({
-			message: 'No reviews were found for that set.',
-		} as GETSetsSetUUIDReviewsReplyError);
-		return;
-	}
 
 	// Return reviews
 	rep.status(200).send(
