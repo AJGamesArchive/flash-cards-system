58bbea0a9d1ebdb0547709d1c5420ac7e5fb4477
AJGamesArchive <68122156+AJGamesArchive@users.noreply.github.com>
2024-12-01 21:24:07 +0000

    Client: Implemented Lazy Loading At Router Level, Polished Flashcard CSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,23 @@
 // Imports
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import useAuthListener, { UseAuthListenerHook, AuthStates } from './hook/auth/UseAuthListener';
 import PageError from './components/core/PageError';
 import PageLoading from './components/core/PageLoading';
-import LoginPage from './pages/login/Login';
-import WelcomePage from './pages/welcome/Welcome';
-import BrowseFlashcardSetsPage from './pages/browse-flashcard-sets/BrowseFlashcardSets';
-import MySetsPage from './pages/my-sets/MySets';
-import SetsEditorPage from './pages/sets-editor/SetEditor';
-import SetReviewsPage from './pages/set-reviews/SetReviews';
-import ReviseFlashcardsPage from './pages/revise-flashcards/ReviseFlashcards';
-import MyCollectionsPage from './pages/my-collections/MyCollections';
-import CollectionSetsPage from './pages/collection-sets/CollectionSets';
-import ViewHiddenFlashcardsPage from './pages/view-hidden-flashcards/ViewHiddenFlashcards';
-import AdminPage from './pages/admin/Admin';
+
+// Lazy load pages
+const LoginPage = lazy(() => import('./pages/login/Login'));
+const WelcomePage = lazy(() => import('./pages/welcome/Welcome'));
+const BrowseFlashcardSetsPage = lazy(() => import('./pages/browse-flashcard-sets/BrowseFlashcardSets'));
+const MySetsPage = lazy(() => import('./pages/my-sets/MySets'));
+const SetsEditorPage = lazy(() => import('./pages/sets-editor/SetEditor'));
+const SetReviewsPage = lazy(() => import('./pages/set-reviews/SetReviews'));
+const ReviseFlashcardsPage = lazy(() => import('./pages/revise-flashcards/ReviseFlashcards'));
+const MyCollectionsPage = lazy(() => import('./pages/my-collections/MyCollections'));
+const CollectionSetsPage = lazy(() => import('./pages/collection-sets/CollectionSets'));
+const ViewHiddenFlashcardsPage = lazy(() => import('./pages/view-hidden-flashcards/ViewHiddenFlashcards'));
+const AdminPage = lazy(() => import('./pages/admin/Admin'));
 
 /**
  * React function to render the core APP and handle app routing
@@ -39,20 +41,22 @@ const App: React.FC = () => {
     );
   } else if(authListener.authState === AuthStates.Permitted) {
     return (
-      <Routes>
-        <Route path="/" Component={LoginPage}/>
-        <Route path="/login" Component={LoginPage}/>
-        <Route path="/welcome" Component={WelcomePage}/>
-        <Route path="/browse-flashcard-sets" Component={BrowseFlashcardSetsPage}/>
-        <Route path="/my-sets" Component={MySetsPage}/>
-        <Route path="/my-sets/sets-editor/:setUUID" Component={SetsEditorPage}/>
-        <Route path="/sets/:setUUID/reviews" Component={SetReviewsPage}/>
-        <Route path="/revise-flashcards/:setUUID/:query" Component={ReviseFlashcardsPage}/>
-        <Route path="/my-collections" Component={MyCollectionsPage}/>
-        <Route path="/my-collections/:collectionUUID/:collectionName/sets" Component={CollectionSetsPage}/>
-        <Route path="/view-hidden-flashcards" Component={ViewHiddenFlashcardsPage}/>
-        <Route path="/admin-panel" Component={AdminPage}/>
-      </Routes>
+      <Suspense fallback={<PageLoading/>}>
+        <Routes>
+          <Route path="/" Component={LoginPage}/>
+          <Route path="/login" Component={LoginPage}/>
+          <Route path="/welcome" Component={WelcomePage}/>
+          <Route path="/browse-flashcard-sets" Component={BrowseFlashcardSetsPage}/>
+          <Route path="/my-sets" Component={MySetsPage}/>
+          <Route path="/my-sets/sets-editor/:setUUID" Component={SetsEditorPage}/>
+          <Route path="/sets/:setUUID/reviews" Component={SetReviewsPage}/>
+          <Route path="/revise-flashcards/:setUUID/:query" Component={ReviseFlashcardsPage}/>
+          <Route path="/my-collections" Component={MyCollectionsPage}/>
+          <Route path="/my-collections/:collectionUUID/:collectionName/sets" Component={CollectionSetsPage}/>
+          <Route path="/view-hidden-flashcards" Component={ViewHiddenFlashcardsPage}/>
+          <Route path="/admin-panel" Component={AdminPage}/>
+        </Routes>
+      </Suspense>
     );
   } else {
     return (
